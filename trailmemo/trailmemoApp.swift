342950cb24b0e901eb9a471c2712ad8a8dc1624b
tom <tpfitz42@gmail.com>
2025-12-09 18:12:33 -0700

    Initial Commit

---
+++ trailmemo/trailmemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  trailmemoApp.swift
+//  trailmemo
+//
+//  Created by Thomas Fitzgerald on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct trailmemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
